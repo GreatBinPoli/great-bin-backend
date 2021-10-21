10b666f5e93f6699ea89b2d156c23395282d78a9
Nicolás Pinzón <apinzo1@bancodebogota.com.co>
2021-10-21 12:41:43 -0500

    Update UserController.java
    Fix delete method

--- src/main/java/co/edu/poli/greatbin/controller/UserController.java
+++ src/main/java/co/edu/poli/greatbin/controller/UserController.java
@@ -3,6 +3,7 @@ package co.edu.poli.greatbin.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import co.edu.poli.greatbin.model.User;
 import co.edu.poli.greatbin.repository.UserRepository;
 
+@Transactional
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
