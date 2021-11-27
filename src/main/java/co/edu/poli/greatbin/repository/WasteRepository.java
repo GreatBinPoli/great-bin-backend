4ae4339a48be379123ac9168d84c818ccb1a8831
Nicolás Pinzón <apinzo1@bancodebogota.com.co>
2021-11-26 22:04:09 -0500

    Add post waste and list

--- src/main/java/co/edu/poli/greatbin/repository/WasteRepository.java
+++ src/main/java/co/edu/poli/greatbin/repository/WasteRepository.java
@@ -1,9 +1,14 @@
 package co.edu.poli.greatbin.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import co.edu.poli.greatbin.model.Bag;
 import co.edu.poli.greatbin.model.Waste;
 
 public interface WasteRepository extends JpaRepository<Waste, String> {
+	
+	List<Waste> findByBag(Bag bag);
 
 }
